c8f39628df87c75206f25b05d44a9a167f06b1e7
Lungsom Lamnio <lungsomlamnio@gmail.com>
2025-02-16 18:41:39 +0530

    frontend and backend connected successfully

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.join(__dirname, "../.env") });
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("root page");
+  res.send("Backend is Running!");
 });
 
-app.listen(port, () => {
-  console.log("server running");
+app.listen(PORT, () => {
+  console.log(`server is running on port http://localhost:${PORT}`);
 });
